d674a143106a81b70de98cd4788962da1a381db8
Eli Marschner <eli@nfcm8.(none)>
2010-12-14 18:03:10 -0800

    post-publishing fixes for redirect from root, and bug where entire database was queried every time the site was loaded w/out a project

--- web/main.py
+++ web/main.py
@@ -36,8 +36,7 @@ class DateEncoder(json.JSONEncoder):
 
 class Index:
   def GET(self):
-    """ Show page """
-    return "Hello, World!"
+    raise web.seeother('/static/map.html?presentationMode=1')
 
 class Test:
   def GET(self):
@@ -78,80 +77,81 @@ class Query:
     linkMaxScale = False
     #logging.info(params.keys())
 
+    results = {'links': [], 'locations': [], 'stats': {}}
     if params.has_key('project'):
       query['project'] = params.project
-    if params.has_key('lat_min') and params.has_key('lat_max'):
-      query['lat'] = {"$gt" : float(params.lat_min), "$lt" : float(params.lat_max)}
-    if params.has_key('long_max') and params.has_key('long_min'):
-      query['long'] = {"$gt" : float(params.long_min), "$lt" : float(params.long_max)}
-    if params.has_key('date_start') and params.has_key('date_end'):
-      date_start = params.date_start.split('/')
-      date_end = params.date_end.split('/')
-      # expected web input: MM/DD/YYYY
-      # datetime input: year, month, day
-      query['date'] = {"$gt" : datetime(int(date_start[2]), int(date_start[0]), int(date_start[1])), 
-              "$lt" : datetime(int(date_end[2]), int(date_end[0]), int(date_end[1])) }
-    if params.has_key('linkAggregation'):
-      if params['linkAggregation'] == "True":
-        linkAggregation = True
-    if params.has_key('linkMaxScale'):
-      linkMaxScale = params['linkMaxScale']
-    #logging.info(query)
+      if params.has_key('lat_min') and params.has_key('lat_max'):
+        query['lat'] = {"$gt" : float(params.lat_min), "$lt" : float(params.lat_max)}
+      if params.has_key('long_max') and params.has_key('long_min'):
+        query['long'] = {"$gt" : float(params.long_min), "$lt" : float(params.long_max)}
+      if params.has_key('date_start') and params.has_key('date_end'):
+        date_start = params.date_start.split('/')
+        date_end = params.date_end.split('/')
+        # expected web input: MM/DD/YYYY
+        # datetime input: year, month, day
+        query['date'] = {"$gt" : datetime(int(date_start[2]), int(date_start[0]), int(date_start[1])), 
+                "$lt" : datetime(int(date_end[2]), int(date_end[0]), int(date_end[1])) }
+      if params.has_key('linkAggregation'):
+        if params['linkAggregation'] == "True":
+          linkAggregation = True
+      if params.has_key('linkMaxScale'):
+        linkMaxScale = params['linkMaxScale']
+      logging.info(query)
 
-    cursor = db.commits.find(query)
-    if params.has_key('sort') and params.sort == "1":
-      cursor = cursor.sort("date", 1)
-    results = {'links': [], 'locations': [], 'stats': {}}
-    seen = set()
-    locations = {}
-    commit_count = 0
-    author_count = 0
-    link_count = 0
-    links = defaultdict(int)
-    for c in cursor:
-      if (not c['sha1'] in seen) and c.has_key('lat') and c.has_key('long'):
-        commit_count += 1
-        geo = (c['lat'], c['long'])
-        author = c['author']
-        location = ''
-        if 'location' in c:
-          location = c['location']
-        else:
-          location = None
+      cursor = db.commits.find(query)
+      if params.has_key('sort') and params.sort == "1":
+        cursor = cursor.sort("date", 1)
+      seen = set()
+      locations = {}
+      commit_count = 0
+      author_count = 0
+      link_count = 0
+      links = defaultdict(int)
+      for c in cursor:
+        if (not c['sha1'] in seen) and c.has_key('lat') and c.has_key('long'):
+          commit_count += 1
+          geo = (c['lat'], c['long'])
+          author = c['author']
+          location = ''
+          if 'location' in c:
+            location = c['location']
+          else:
+            location = None
 
-        if geo not in locations:
-          locations[geo] = {'_locations': []}
+          if geo not in locations:
+            locations[geo] = {'_locations': []}
 
-        if author not in locations[geo]:
-          if location: locations[geo]['_locations'].append(location)
-          locations[geo][author] = True
-          author_count += 1
+          if author not in locations[geo]:
+            if location: locations[geo]['_locations'].append(location)
+            locations[geo][author] = True
+            author_count += 1
 
-        for p_sha1 in c['parents']:
-          p = db.commits.find_one({'sha1': p_sha1})
-          if p and p.has_key('lat') and p.has_key('long') and (p['lat'] != c['lat']) and (p['long'] != c['long']) :
-            if linkAggregation:
-            #results['links'].append([c, p])
-              key = ((c['long'], c['lat']),(p['long'], p['lat']), c['date'])
-              links[key] = links[key] + 1
-            else:
-              results['links'].append(((c['long'], c['lat']),(p['long'], p['lat']), c['date']))
-              link_count += 1
-        seen.add(c['sha1'])
+          for p_sha1 in c['parents']:
+            p = db.commits.find_one({'sha1': p_sha1})
+            if p and p.has_key('lat') and p.has_key('long') and (p['lat'] != c['lat']) and (p['long'] != c['long']) :
+              if linkAggregation:
+              #results['links'].append([c, p])
+                key = ((c['long'], c['lat']),(p['long'], p['lat']), c['date'])
+                links[key] = links[key] + 1
+              else:
+                results['links'].append(((c['long'], c['lat']),(p['long'], p['lat']), c['date']))
+                link_count += 1
+          seen.add(c['sha1'])
 
-    for key, val in locations.iteritems():
-      hash = {'lat': key[0], 'long': key[1]}
-      hash['loc_count'] = max(len(val) - 1, 0)
-      hash['authors'] = [e for e in val.keys() if e != '_locations']
-      hash['locations'] = [loc for loc in Set(([e for e in val['_locations']]))]
-      results['locations'].append(hash)
+      for key, val in locations.iteritems():
+        hash = {'lat': key[0], 'long': key[1]}
+        hash['loc_count'] = max(len(val) - 1, 0)
+        hash['authors'] = [e for e in val.keys() if e != '_locations']
+        hash['locations'] = [loc for loc in Set(([e for e in val['_locations']]))]
+        results['locations'].append(hash)
 
-    results['stats']['commit_count'] = commit_count
-    results['stats']['author_count'] = author_count
-    results['stats']['link_count'] = link_count
+      results['stats']['commit_count'] = commit_count
+      results['stats']['author_count'] = author_count
+      results['stats']['link_count'] = link_count
 
-    if linkAggregation:
-      results['links'] = links.items()
+      if linkAggregation:
+        results['links'] = links.items()
+
     return "jsonpcallback("+json.dumps(results, cls=DateEncoder)+")"
 
 class Stats:
