0464fdb344fe20a22f1ccdd43e17f068b9a55a0d
Brandon Heller <brandonh@stanford.edu>
2011-03-29 01:26:46 -0700

    Add ability to read in files and write GEXF output

--- analysis/geograph/graph.py
+++ analysis/geograph/graph.py
@@ -8,6 +8,7 @@
 import json
 from operator import itemgetter
 import os
+import pickle
 import sys
 
 import networkx as nx
@@ -18,6 +19,10 @@ import geo
 
 
 SEP = "\t"  # default separator
+
+# Add edges only when both nodes have valid locations
+VALID_LOCATIONS_ONLY = True
+
 
 class GeoGraph(nx.DiGraph):
     """GeoGraph: a directed, weighted graph with a geographic embedding.
@@ -39,8 +44,18 @@ class GeoGraph(nx.DiGraph):
     # Extension for pickled GeoGraphs
     EXT = '.geo'
 
-    def __init__(self):
+    def __init__(self, filename = None):
         super(GeoGraph, self).__init__()
+
+    @staticmethod
+    def read(filename):
+        """Return GeoGraph from pickled file if filename exists."""
+        if not os.path.exists(filename):
+            raise Exception("no file found: %s" % filename)
+        else:
+            print " reading input graph from %s" % filename
+            file = open(filename, 'r')
+            return pickle.load(file)
 
     @staticmethod
     def get_key(data):
@@ -116,3 +131,73 @@ class GeoGraph(nx.DiGraph):
         filename = path + ext
         print "writing GeoGraph to %s" % filename
         nx.write_gpickle(self, filename)
+
+    def write_gexf(self, path, ext = '.gexf', max_edges = None):
+        """Write as a GEXF output, suitable for Gephi input."""
+        filename = path + ext
+        print "writing GeoGraph as GEXF to %s" % filename
+        gexf = self.geo_gexf_graph(max_edges)
+        #gexf_path = os.path.join(name, name + '_' + '_'.join(append) + ext)
+        nx.write_gexf(gexf, filename)
+
+    @staticmethod
+    def gexf_viz(node):
+        """Return viz param suitable for usage in Gephi."""
+        lat, long = node
+        return {'position': {'x': long, 'y': lat, 'z': 0}}
+
+    def geo_gexf_graph(self, max_edges = None):
+        '''Returns NX graph w/weights between city names plus vis params.'''
+        good_names = 0
+        good_edges = 0
+        c = nx.DiGraph()
+        edge_weight_total = 0
+        edge_weights = []
+        max_edge_weight = 0
+        # Iterate over every node: O(E), where E is # edges.
+        print "self.number_of_edges: %i" % self.number_of_edges()
+
+        edges_seen = 0
+        for src_node, dst_node in self.edges():
+            if max_edges and (edges_seen == max_edges):
+                break
+            else:
+                edges_seen += 1
+
+            if self.has_edge(src_node, dst_node):
+                edge_weight = self[src_node][dst_node]["weight"]
+            else:
+                edge_weight = 0
+
+            # FIXME: ugly hack to avoid writing a function to find the most
+            # popular in a dict.
+            src_name = self.node[src_node]['locations'].keys()[0]
+            dst_name = self.node[dst_node]['locations'].keys()[0]
+
+            if src_name:
+                good_names += 1
+
+            if dst_name:
+                good_names += 1
+
+            if VALID_LOCATIONS_ONLY and src_name and dst_name:
+                c.add_edge(src_name, dst_name)
+                c[src_name][dst_name]["weight"] = edge_weight
+                c.node[src_name] = {'viz': self.gexf_viz(src_node)}
+                c.node[dst_name] = {'viz': self.gexf_viz(dst_node)}
+                c[src_name][dst_name]["weight"] = edge_weight
+                edge_weight_total += edge_weight
+                edge_weights.append(edge_weight)
+                good_edges += 1
+                if edge_weight > max_edge_weight:
+                    max_edge_weight = edge_weight
+
+        for node in c.nodes():
+            #print "node", c.node[node]
+            pass
+        print "edge_weight_total: %i" % edge_weight_total
+        print "edges_seen: %i" % edges_seen
+        print "good_names: %i" % good_names
+        print "good_edges: %i" % good_edges
+        print "max edge weight: %i" % max_edge_weight
+        return c
